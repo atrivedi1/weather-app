e195ebd0c8d32f84b2de6ce9f242953b8f232dbb
Akash Trivedi <akash@citizencode.io>
2016-10-19 20:46:32 -0700

    Successfully grab information from openweathermap API

--- client/controllers.js
+++ client/controllers.js
@@ -9,7 +9,23 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
-
+  var apiKey = '4f1a736c3d25da664b1152de4574f2d5';
   $scope.city = cityService.city;
+  $scope.days = $routeParams.days || '2';
+
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=" + apiKey,
+    { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+  $scope.convertToFahrenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);
+  }
+
+  $scope.convertToDate = function(dt) {
+    return new Date(dt * 1000);
+  };
+
+  console.log($scope.weatherResult)
 
 }]);
