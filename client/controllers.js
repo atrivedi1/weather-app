113614f5241200872204f5203a6cdc1be3101315
Akash Trivedi <akash@citizencode.io>
2016-10-19 17:41:34 -0700

    Create cityService and inject into controllers to connect views

--- client/controllers.js
+++ client/controllers.js
@@ -1,9 +1,15 @@
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
 
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  })
 
 }]);
 
-weatherApp.controller('forecastController',  ['$scope', '$resource', '$routeParams',  function($scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
+  $scope.city = cityService.city;
 
 }]);
